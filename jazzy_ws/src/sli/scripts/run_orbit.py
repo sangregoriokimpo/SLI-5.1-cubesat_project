ea8d14ce82dbcc8ec13c1aa9c93384a2bbd4eba7
sangregoriokimpo <144968837+sangregoriokimpo@users.noreply.github.com>
2026-03-29 15:03:46 -0700

    feat: add directory for project

--- jazzy_ws/src/sli/scripts/run_orbit.py
+++ jazzy_ws/src/sli/scripts/run_orbit.py
@@ -1,11 +1,17 @@
 import os
 from isaacsim import SimulationApp
+from ament_index_python.packages import get_package_share_directory
+
 
 simulation_app = SimulationApp({"headless": False})
 
 from isaacsim.core.utils.stage import open_stage, is_stage_loading
 
-usd_path = os.path.expanduser("~/Documents/SLI/orbitTest3/orbitTest3A.usd")
+pkg = get_package_share_directory('sli')
+
+usd_path = os.path.join(pkg, 'usd_files','earth', 'earthmodel.usd')
+config_path = os.path.join(pkg, 'config', 'orbit_scene.yaml')
+
 open_stage(usd_path)
 while is_stage_loading():
     simulation_app.update()
